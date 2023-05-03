271c8fe29d2cb287edaf1761139c8bc5bb108797
ily-cloudy <cloudyy.ily@gmail.com>
2023-05-04 01:44:06 +0200

    On branch main Changes to be committed: 	modified:   src/environments/EnvData.java

--- src/environments/EnvData.java
+++ src/environments/EnvData.java
@@ -11,24 +11,22 @@ public class EnvData {
 
         layout = new HashMap<int[], String>();
 
-        // row 14 
+        // row 13 
         layout.put(new int[]{0,13},"dark 1x1");
         layout.put(new int[]{1,13},"orange rect 2x1");
         layout.put(new int[]{3,13},"dark 1x1");
-        layout.put(new int[]{4,13},"grass 2x1");
-        layout.put(new int[]{6,13},"dark 1x1");
-        layout.put(new int[]{7,13},"dark 1x1");
-        layout.put(new int[]{8,13},"dark 1x1");
-        layout.put(new int[]{9,13},"dark 1x1");
-        layout.put(new int[]{10,13},"dark 1x1");
-        layout.put(new int[]{11,13},"dark 1x1");
-        layout.put(new int[]{12,13},"dark 1x1");
-        layout.put(new int[]{13,13},"dark 1x1");
-        layout.put(new int[]{14,13},"dark 1x1");
-        layout.put(new int[]{15,13},"dark 1x1");
-        layout.put(new int[]{16,13},"dark 1x1");
-        layout.put(new int[]{17,13},"dark 1x1");
-        layout.put(new int[]{18,13},"dark 1x1");
+        layout.put(new int[]{4,13},"grass1 1x1");
+        layout.put(new int[]{5,13},"orange grass 3x1");
+        layout.put(new int[]{8,13},"c1 top right 1x1");
+        layout.put(new int[]{9,13},"spikes up1 1x1");
+        layout.put(new int[]{10,13},"spikes up2 1x1");
+        layout.put(new int[]{11,13},"spikes up1 1x1");
+        layout.put(new int[]{12,13},"spikes up1 1x1");
+        layout.put(new int[]{13,13},"spikes up2 1x1");
+        layout.put(new int[]{14,13},"spikes up2 1x1");
+        layout.put(new int[]{15,13},"leftvine3 1x1");
+        layout.put(new int[]{15,13},"spikes up1 1x1");
+        layout.put(new int[]{16,13},"rect 3x1");
         layout.put(new int[]{19,13},"dark 1x1");
         layout.put(new int[]{20,13},"dark 1x1");
         layout.put(new int[]{21,13},"dark 1x1");
@@ -37,8 +35,106 @@ public class EnvData {
         layout.put(new int[]{24,13},"dark 1x1");
         layout.put(new int[]{25,13},"dark 1x1");
                 
-        // row 13
-    }
+        // row 12
+        layout.put(new int[]{0,12},"dark+vine 1x1");
+        layout.put(new int[]{1,12},"dark 1x1");
+        layout.put(new int[]{2,12},"dark 1x1");
+        layout.put(new int[]{3,12},"dark 1x1");
+        layout.put(new int[]{4,12},"rightvine2 1x1");
+        layout.put(new int[]{4,12},"topgrass3 1x1");
+        layout.put(new int[]{5,12},"topgrass1 1x1");
+        layout.put(new int[]{6,12},"bush3 1x1");
+        layout.put(new int[]{7,12},"topgrass2 1x1");
+        layout.put(new int[]{8,12},"topgrass3 1x1");
+
+        layout.put(new int[]{15,12},"leftvine2 1x1");
+        layout.put(new int[]{16,12},"dark 1x1");
+        layout.put(new int[]{17,12},"dark 1x1");
+        layout.put(new int[]{18,12},"dark 1x1");
+        layout.put(new int[]{19,12},"dark 1x1");
+        layout.put(new int[]{20,12},"dark 1x1");
+        layout.put(new int[]{21,12},"dark 1x1");
+        layout.put(new int[]{22,12},"dark 1x1");
+        layout.put(new int[]{23,12},"dark 1x1");
+        layout.put(new int[]{24,12},"dark 1x1");
+        layout.put(new int[]{25,12},"dark 1x1");
+
+        // row 11
+        layout.put(new int[]{0,11},"c2 top left 1x1");
+        layout.put(new int[]{1,11},"grass 2x1");
+        layout.put(new int[]{3,11},"c1 top right 1x1");
+        layout.put(new int[]{4,11},"rightvine1 1x1");
+        
+        layout.put(new int[]{15,11},"leftvine1 1x1");
+        layout.put(new int[]{16,11},"c1 top left 1x1");
+        layout.put(new int[]{17,11},"dark 1x1");
+        layout.put(new int[]{18,11},"rect3 2x1");
+        layout.put(new int[]{20,11},"dark 1x1");
+        layout.put(new int[]{21,11},"dark+vine 1x1");
+        layout.put(new int[]{22,11},"dark 1x1");
+        layout.put(new int[]{23,11},"dark 1x1");
+        layout.put(new int[]{24,11},"pond 6x1");
+
+
+        // row 10
+        layout.put(new int[]{2,10},"topgrass1 1x1");
+        layout.put(new int[]{3,10},"bush1 1x1");
+
+        layout.put(new int[]{6,10},"lights 1x1");
+        layout.put(new int[]{7,10},"hang grass 1x1");
+        layout.put(new int[]{8,10},"lights 1x2");
+
+        layout.put(new int[]{16,10},"bush2 1x1");
+        
+        // row 9
+        layout.put(new int[]{0,9},"rock 2x2");
+
+        layout.put(new int[]{5,9},"leftvine3 1x1");
+        layout.put(new int[]{6,9},"c1 bot left 1x1");
+        layout.put(new int[]{7,9},"dark 1x1");
+        layout.put(new int[]{8,9},"c1 bot right 1x1");
+        layout.put(new int[]{9,9},"rightvine1 1x1");
+
+        layout.put(new int[]{11,9},"hang grass 1x1");
+        layout.put(new int[]{12,9},"lights 1x2");
+        layout.put(new int[]{13,9},"lights 1x1");
+        
+        layout.put(new int[]{23,9},"bush on rock 5x2");
+
+        // row 8
+        layout.put(new int[]{5,8},"leftvine2 1x1");
+        layout.put(new int[]{6,8},"c1 top left 1x1");
+        layout.put(new int[]{7,8},"grass1 1x1");
+        layout.put(new int[]{8,8},"c2 top right 1x1");
+        layout.put(new int[]{9,8},"rightvine2 1x1");
+
+        layout.put(new int[]{10,8},"leftvine3 1x1");
+        layout.put(new int[]{11,8},"c1 bot left 1x1");
+        layout.put(new int[]{12,8},"dark 1x1");
+        layout.put(new int[]{13,8},"c1 bot right 1x1");
+        layout.put(new int[]{14,8},"rightvine2 1x1");
+
+        // row 7
+        layout.put(new int[]{6,7},"topgrass1 1x1");
+        layout.put(new int[]{7,7},"stub 1x1");
+        layout.put(new int[]{8,7},"tiny bush 1x1");
+
+        layout.put(new int[]{10,7},"leftvine2 1x1");
+        layout.put(new int[]{11,7},"c2 top left 1x1");
+        layout.put(new int[]{12,7},"grass2 1x1");
+        layout.put(new int[]{13,7},"c2 top right 1x1");
+        layout.put(new int[]{14,7},"rightvine1 1x1");
+
+        // top 6
+        layout.put(new int[]{11,6},"bush2 1x1");
+        layout.put(new int[]{12,6},"bush1 1x1");
+        layout.put(new int[]{13,6},"topgrass2 1x1");
+
+
+        // row 2
+        layout.put(new int[]{17,2},"tree 7x9");
+
+        }
 
 
 
