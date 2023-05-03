0e3a6d92f81cb62290ed5a344a4079fd353e0fff
ily-cloudy <cloudyy.ily@gmail.com>
2023-05-03 02:11:50 +0200

    On branch main Changes to be committed: 	modified:   res/tileset.png 	modified:   src/environments/EnvManager.java

--- src/environments/EnvManager.java
+++ src/environments/EnvManager.java
@@ -8,7 +8,10 @@ import main.Game;
 import utility.LoadSave;
 
 public class EnvManager {
-    
+
+    private int dim_x, dim_y;
+    private String sprite = "spikes up2 1x1";
+
     private Game game;
 //  private BufferedImage env_sprite;
     HashMap<String, BufferedImage> env_sprite_map = new HashMap<>();
@@ -33,10 +36,25 @@ public class EnvManager {
         env_sprite_map.put("pond 6x1", img.getSubimage(6*16, 9*16, 6*16, 1*16));
         env_sprite_map.put("grass 2x1", img.getSubimage(12*16, 9*16, 2*16, 1*16));
         env_sprite_map.put("orange grass 3x1", img.getSubimage(14*16, 9*16, 3*16, 1*16));
+        env_sprite_map.put("cornersquare1 1x1", img.getSubimage(0*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("cornersquare2 1x1", img.getSubimage(1*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("rect1 2x1", img.getSubimage(2*16, 10*16, 2*16, 1*16));
+        env_sprite_map.put("orange rect 2x1", img.getSubimage(4*16, 10*16, 2*16, 1*16));
+        env_sprite_map.put("rect2 2x1", img.getSubimage(6*16, 10*16, 2*16, 1*16));
+        env_sprite_map.put("rect3 2x1", img.getSubimage(8*16, 10*16, 2*16, 1*16));
         // one-way platforms
-        
+        env_sprite_map.put("oneway left 2x1", img.getSubimage(5*16, 4*16, 2*16, 1*16));
+        env_sprite_map.put("oneway right 2x1", img.getSubimage(8*16, 4*16, 2*16, 1*16));
+        env_sprite_map.put("oneway 1x1", img.getSubimage(7*16, 4*16, 1*16, 1*16));
         // traps
-
+        env_sprite_map.put("spikes up1 1x1", img.getSubimage(10*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes up2 1x1", img.getSubimage(11*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes right1 1x1", img.getSubimage(12*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes right2 1x1", img.getSubimage(13*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes down1 1x1", img.getSubimage(14*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes down2 1x1", img.getSubimage(15*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes left1 1x1", img.getSubimage(16*16, 10*16, 1*16, 1*16));
+        env_sprite_map.put("spikes left2 1x1", img.getSubimage(16*16, 10*16, 1*16, 1*16));
         // trees
         env_sprite_map.put("tree 7x9", img.getSubimage(176, 0, 112, 144));
         env_sprite_map.put("tree 4x4", img.getSubimage(0, 5*16, 4*16, 4*16));
@@ -47,11 +65,29 @@ public class EnvManager {
         env_sprite_map.put("hang grass 1x1", img.getSubimage(1*16, 3*16, 1*16, 1*16));
         env_sprite_map.put("tiny bush 1x1", img.getSubimage(0*16, 4*16, 1*16, 1*16));
         env_sprite_map.put("bush1 1x1", img.getSubimage(1*16, 4*16, 1*16, 1*16));
+        env_sprite_map.put("bush2 1x1", img.getSubimage(9*16, 7*16, 1*16, 1*16));
+        env_sprite_map.put("bush3 1x1", img.getSubimage(9*16, 8*16, 1*16, 1*16));
+        env_sprite_map.put("bush 3x2", img.getSubimage(7*16, 5*16, 3*16, 2*16));
+        env_sprite_map.put("bush on rock 5x2", img.getSubimage(4*16, 7*16, 5*16, 2*16));
         env_sprite_map.put("rock 2x2", img.getSubimage(3*16, 3*16, 2*16, 2*16));
-    }
+        env_sprite_map.put("stub 1x1", img.getSubimage(4*16, 5*16, 1*16, 1*16));
+        env_sprite_map.put("topgrass1 1x1", img.getSubimage(4*16, 6*16, 1*16, 1*16));
+        env_sprite_map.put("topgrass2 1x1", img.getSubimage(5*16, 6*16, 1*16, 1*16));
+        env_sprite_map.put("topgrass3 1x1", img.getSubimage(6*16, 6*16, 1*16, 1*16));
+        env_sprite_map.put("cornervine1 1x1", img.getSubimage(5*16, 5*16, 1*16, 1*16));
+        env_sprite_map.put("cornervine2 1x1", img.getSubimage(6*16, 5*16, 1*16, 1*16));
 
+
+
+        // automatic scaling constants
+        // look i know this is a massive bodge but i was tired and it works
+        dim_x = Integer.parseInt(String.valueOf(sprite.charAt(sprite.length()-3))); 
+        dim_y = Integer.parseInt(String.valueOf(sprite.charAt(sprite.length()-1))); 
+    }
+    
     public void draw(Graphics g) {
-        g.drawImage(env_sprite_map.get("bush1 1x1"), 100, 100, 1*4*16, 1*4*16, null);
+    
+        g.drawImage(env_sprite_map.get(sprite), 100, 100, dim_x*4*16, dim_y*4*16, null);
     }
 
     public void update() {
